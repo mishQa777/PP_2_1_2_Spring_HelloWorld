f00435e169b690151c2c4e10bc9644a588250cea
Mikhail <potemko23@yandex.ru>
2022-04-17 23:04:37 +0300

    zadacha 2/1/2
    zadacha 2/1/2

---
+++ src/main/java/Cat.java
@@ -0,0 +1,12 @@
+public class Cat {
+    private String voice;
+
+    public String getMessage() {
+        return voice;
+    }
+
+    public void setMessage(String message) {
+        this.voice = message;
+    }
+
+}
